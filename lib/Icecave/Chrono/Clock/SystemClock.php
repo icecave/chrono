10957f0e8af4c6b47f18165a25603b8c240cbdb7
James Harris <stoffle@gmail.com>
2013-01-21 16:17:49 +1000

    Implemented SystemClock.

--- lib/Icecave/Chrono/Clock/SystemClock.php
+++ lib/Icecave/Chrono/Clock/SystemClock.php
@@ -1,57 +1,48 @@
 <?php
-// namespace Icecave\Chrono\Clock;
-// 
-// use Icecave\Isolator\Isolator;
-// 
-// class SystemClock extends AbstractClock
-// {
-//     /**
-//      * @param Isolator|null $isolator
-//      */
-//     public function __construct(Isolator $isolator = null)
-//     {
-//         $this->isolator = Isolator::get($isolator);
-//     }
-// 
-//     /**
-//      * @return TimeZone The local timezone.
-//      */
-//     public function timeZone()
-//     {
-//         list($seconds, $minutes, $hours, $day, $month, $year, $weekDay, $yearDay, $isDst) = $this->localTimeInfo();
-//         $offset = $this->isolator->date('Z', $this->unixTime());
-// 
-//         return new TimeZone($offset, $isDst ? true : false);
-//     }
-// 
-//     /**
-//      * @return integer The current time as a unix timestamp.
-//      */
-//     public function unixTime()
-//     {
-//         return $this->isolator->time();
-//     }
-// 
-//     /**
-//      * @return array<integer> A tuple containing time information, as per {@see localtime()}.
-//      */
-//     protected function localTimeInfo()
-//     {
-//         return $this->isolator->localtime($this->unixTime());
-//     }
-// 
-//     /**
-//      * @return array<integer> A tuple containing time information, as per {@see localtime()}, but for the UTC timezone.
-//      */
-//     protected function utcTimeInfo()
-//     {
-//         $parts   = $this->isolator->gmdate('s,i,H,d,m,Y,w,z', $this->unixTime());
-//         $parts   = explode(',', $parts);
-//         $parts   = array_map('intval', $parts);
-//         $parts[] = 0; // isDst
-// 
-//         return $parts;
-//     }
-// 
-//     private $isolator;
-// }
+namespace Icecave\Chrono\Clock;
+
+use Icecave\Chrono\TimeZone;
+use Icecave\Isolator\Isolator;
+
+class SystemClock extends AbstractClock
+{
+    /**
+     * @param Isolator|null $isolator
+     */
+    public function __construct(Isolator $isolator = null)
+    {
+        $this->isolator = Isolator::get($isolator);
+    }
+
+    /**
+     * @return integer The current time as a unix timestamp.
+     */
+    public function unixTime()
+    {
+        return $this->isolator->time();
+    }
+
+    /**
+     * @return array<integer> A tuple containing time information, as per {@see localtime()}, plus the timezone offset in seconds.
+     */
+    protected function currentLocalTimeInfo()
+    {
+        $parts = $this->isolator->date('s,i,H,d,m,Y,w,z,I,Z', $this->unixTime());
+        $parts = explode(',', $parts);
+        $parts = array_map('intval', $parts);
+        return $parts;
+    }
+
+    /**
+     * @return array<integer> A tuple containing time information, as per {@see localtime()}, plus the timezone offset in seconds, but for the UTC timezone.
+     */
+    protected function currentUtcTimeInfo()
+    {
+        $parts = $this->isolator->gmdate('s,i,H,d,m,Y,w,z,0,0', $this->unixTime());
+        $parts = explode(',', $parts);
+        $parts = array_map('intval', $parts);
+        return $parts;
+    }
+
+    private $isolator;
+}
--- test/suite/Icecave/Chrono/Clock/SystemClockTest.php
+++ test/suite/Icecave/Chrono/Clock/SystemClockTest.php
@@ -16,12 +16,37 @@ class SystemClockTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->_isolator = Phake::mock(get_class(Isolator::get()));
-        // $this->_clock = new SystemClock($this->_isolator);
+        $this->_clock = new SystemClock($this->_isolator);
+
+        Phake::when($this->_isolator)
+            ->time()
+            ->thenReturn(1358748637);
+
+        Phake::when($this->_isolator)
+            ->date('s,i,H,d,m,Y,w,z,I,Z', 1358748637)
+            ->thenReturn('37,10,16,21,01,2013,1,20,1,36000');
+
+        Phake::when($this->_isolator)
+            ->gmdate('s,i,H,d,m,Y,w,z,0,0', 1358748637)
+            ->thenReturn('37,10,06,21,01,2013,1,20,0,0');
     }
 
-    public function testFoo()
+    public function testUnixTime()
     {
-        $this->assertTrue(true);
+        $this->assertSame(1358748637, $this->_clock->unixTime());
     }
-    
+
+    public function testCurrentLocalTimeInfo()
+    {
+        $result = $this->_clock->localDateTime();
+        $expected = new DateTime(2013, 1, 21, 16, 10, 37, new TimeZone(36000, true));
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testCurrentUtcTimeInfo()
+    {
+        $result = $this->_clock->utcDateTime();
+        $expected = new DateTime(2013, 1, 21, 6, 10, 37, new TimeZone);
+        $this->assertEquals($expected, $result);
+    }
 }
